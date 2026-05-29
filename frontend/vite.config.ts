965ae30dae800764b9edd80cd3250833374426bc
Novavida Group <mikecfurman@gmail.com>
2026-05-29 10:50:02 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
-  import path from 'path'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-    plugins: [react()],
-        resolve: { alias: { '@': path.resolve(__dirname, './src') } }
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
